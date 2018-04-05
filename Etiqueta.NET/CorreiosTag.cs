d47fc4632ad08f7b534f364c47cc8b132b0dd67e
Davi da Silva Fernades <davi.fernandes2012@gmail.com>
2018-04-05 10:57:42 -0300

    teste

--- Etiqueta.NET/CorreiosTag.cs
+++ Etiqueta.NET/CorreiosTag.cs
@@ -15,7 +15,7 @@ namespace Etiqueta.NET.Core
         #region Properties
         public IList<String> PathsList { get; set; }
 
-        public enum Tag
+        public enum Tags
         {
             Quatro,
             Seis
@@ -42,6 +42,7 @@ namespace Etiqueta.NET.Core
             STANDARD
         }
         #endregion
+
         #region Constructor
 
         #endregion
@@ -50,7 +51,7 @@ namespace Etiqueta.NET.Core
         public static void GenerateFour()
         {
 
-            var bmp = GetTemplate(CorreiosTag.Tag.Quatro);
+            var bmp = GetTemplate(CorreiosTag.Tags.Quatro);
             var icon = GetType(CorreiosTag.TagType.EXPRESSA);
             var codRegistroPoint = new RectangleF(320, 15, 96, 96);
 
@@ -70,24 +71,57 @@ namespace Etiqueta.NET.Core
             bmp.Dispose();
           
         }
+
         public static void GenerateSix()
         {
 
-            var bmp = GetTemplate(CorreiosTag.Tag.Seis);
+            var bmp = GetTemplate(CorreiosTag.Tags.Seis);
             var Matrix = GenerateDataMatrix("064340300111706434070001170000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
             var icon = GetType(CorreiosTag.TagType.PREMIUM);
             var codRegistroPoint = new RectangleF(330, 10, 59, 59);
             var dataMatrix = new RectangleF(10, 10, 70, 70);
-            
+            var contratoXY = new RectangleF(165, 38, 200, 22);
+            var tag = new Tag
+            {
+                Contrato = 1234567890,
+                Servico = "Sedex Hoje Amanha e Depois",
+                NF = 1234567,
+                Pedido = "123456789",
+                Peso = 123456,
+                Rastreamento = "PP578197848BR",
+                Destinatario = "teste teste teste teste teste teste teste teste te",
+                EnderecoDest = "endreco destinatario ausente destinatario ausentee",
+                NumeroDest = 123456,
+                ComplementoDest = "complemento complemento comple",
+                BairroDest = "bairro do destinatario ausente destinatario ausent",
+                CEPDest = "12345-678",
+                CidadeDest = "Vila Bela da Santíssima Trindade",
+                UFDest = "SP",
+
+                Remetente = "teste2 teste2 teste2 teste2 teste2 teste2 teste2 2",
+                EndrerecoRemet = "endereco endereco endereco endereco endereco ender", 
+                NumeroRemet = 123456,
+                ComplementoRemet = "complemento complemento comple",
+                BairroRemet = "Bairro Bairro Bairro Bairro Bairro Bairro Bairro B",
+                CEPRemet = "12345-678",
+                CidadeRemet = "Vila Bela da Santíssima Trindade",
+                UFRemet = "SP"
+                
+            };
+
+
+
 
 
             var g = Graphics.FromImage(bmp);
             g.SmoothingMode = SmoothingMode.AntiAlias;
             g.InterpolationMode = InterpolationMode.HighQualityBicubic;
             g.PixelOffsetMode = PixelOffsetMode.HighQuality;
-            var format = "{0}\n{1}\n{2}, {3}\n{4}  {5} - {6}";
+            // var format = "{0}\n{1}\n{2}, {3}\n{4}  {5} - {6}";
             g.DrawImage(icon, codRegistroPoint);
             g.DrawImage(Matrix, dataMatrix);
+            g.DrawString(tag.Contrato.ToString(), new Font("Arial", 9, FontStyle.Bold), Brushes.Black, contratoXY);
+
             //g.DrawString("dsadaas", new Font("Arial", 12, FontStyle.Bold), Brushes.Black, codRegistroPoint);
             var fileName = "label-" + DateTime.Now.ToString("dd-MM-yyyy-hh-mm-ss-fff") + ".jpg";
             g.Flush();
@@ -141,16 +175,16 @@ namespace Etiqueta.NET.Core
             return encoder.EncodeImage(Code, options);
         }
 
-        private static Bitmap GetTemplate(Tag tag)
+        private static Bitmap GetTemplate(Tags tag)
         {
         Bitmap bmp = null;
             switch (tag)
             {
-                case Tag.Quatro:
+                case Tags.Quatro:
                     {
                         bmp = Properties.Resources.EtiquetaAutomatizada_4pFolha; break;
                     }
-                case Tag.Seis:
+                case Tags.Seis:
                     {
                         bmp = Properties.Resources.EtiquetaAutomatizada_6pFolha; break;
                     }
---
+++ Etiqueta.NET/Tag.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etiqueta.NET.Core
+{
+    public class Tag
+    {
+        public int Contrato { get; set; }
+        public string Servico { get; set; }
+        public int NF { get; set; }
+        public string Pedido { get; set; }
+        public string Volume { get; set; }//nao aplicado
+        public int Peso { get; set; }
+        public string Rastreamento { get; set; }
+
+        public string Destinatario { get; set; }
+        public string EnderecoDest { get; set; }
+        public int NumeroDest { get; set; }
+        public string ComplementoDest { get; set; }
+        public string BairroDest { get; set; }
+        public string CEPDest { get; set; }
+        public string CidadeDest { get; set; }
+        public string UFDest { get; set; }
+
+        public string Remetente { get; set; }
+        public string EndrerecoRemet { get; set; }
+        public int NumeroRemet { get; set; }
+        public string ComplementoRemet { get; set; }
+        public string BairroRemet { get; set; }
+        public string CEPRemet { get; set; }
+        public string CidadeRemet { get; set; }
+        public string UFRemet { get; set; }
+    }
+}
