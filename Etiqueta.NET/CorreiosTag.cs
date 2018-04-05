d2c4148e338cbad0a2e0151b02d9b5ce5ef208d8
Davi da Silva Fernades <davi.fernandes2012@gmail.com>
2018-04-05 15:39:50 -0300

    dia 2

--- Etiqueta.NET/CorreiosTag.cs
+++ Etiqueta.NET/CorreiosTag.cs
@@ -1,4 +1,5 @@
-﻿using DataMatrix.net;
+﻿using BarcodeLib;
+using DataMatrix.net;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -74,21 +75,15 @@ namespace Etiqueta.NET.Core
 
         public static void GenerateSix()
         {
-
-            var bmp = GetTemplate(CorreiosTag.Tags.Seis);
-            var Matrix = GenerateDataMatrix("064340300111706434070001170000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
-            var icon = GetType(CorreiosTag.TagType.PREMIUM);
-            var codRegistroPoint = new RectangleF(330, 10, 59, 59);
-            var dataMatrix = new RectangleF(10, 10, 70, 70);
-            var contratoXY = new RectangleF(165, 38, 200, 22);
             var tag = new Tag
             {
                 Contrato = 1234567890,
-                Servico = "Sedex Hoje Amanha e Depois",
+                Servico = "Sedex Hoje Amanha",
                 NF = 1234567,
                 Pedido = "123456789",
                 Peso = 123456,
                 Rastreamento = "PP578197848BR",
+
                 Destinatario = "teste teste teste teste teste teste teste teste te",
                 EnderecoDest = "endreco destinatario ausente destinatario ausentee",
                 NumeroDest = 123456,
@@ -99,16 +94,30 @@ namespace Etiqueta.NET.Core
                 UFDest = "SP",
 
                 Remetente = "teste2 teste2 teste2 teste2 teste2 teste2 teste2 2",
-                EndrerecoRemet = "endereco endereco endereco endereco endereco ender", 
+                EndrerecoRemet = "endereco endereco endereco endereco endereco ender",
                 NumeroRemet = 123456,
                 ComplementoRemet = "complemento complemento comple",
                 BairroRemet = "Bairro Bairro Bairro Bairro Bairro Bairro Bairro B",
                 CEPRemet = "12345-678",
                 CidadeRemet = "Vila Bela da Santíssima Trindade",
                 UFRemet = "SP"
-                
+
             };
 
+            var bmp = GetTemplate(CorreiosTag.Tags.Seis);
+            var Matrix = GenerateDataMatrix("064340300111706434070001170000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+            var icon = GetType(CorreiosTag.TagType.PREMIUM);
+            var imgCodigoBarrasZip = GerarCodBarrasCep(tag.CEPDest);
+            var codRegistroPoint = new RectangleF(338, 10, 59, 59);
+            var dataMatrix = new RectangleF(10, 10, 70, 70);
+            var contratoXY = new RectangleF(155, 38, 200, 22);
+            var servicoXY = new RectangleF(100, 50, 200, 22);
+            var nfXY = new RectangleF(250, 8, 200, 22);
+            var pedidoXY = new RectangleF(271, 23, 200, 22);
+            var pesoXY = new RectangleF(282, 53, 200, 22);
+            var rastreamentoXY = new RectangleF(100, 70, 200, 22);
+            var destinatarioXY = new RectangleF(10, 230, 300, 40);
+            var imgCodbarraXY = new RectangleF(260, 220, 130, 70);
 
 
 
@@ -120,8 +129,22 @@ namespace Etiqueta.NET.Core
             // var format = "{0}\n{1}\n{2}, {3}\n{4}  {5} - {6}";
             g.DrawImage(icon, codRegistroPoint);
             g.DrawImage(Matrix, dataMatrix);
-            g.DrawString(tag.Contrato.ToString(), new Font("Arial", 9, FontStyle.Bold), Brushes.Black, contratoXY);
+            g.DrawImage(imgCodigoBarrasZip, imgCodbarraXY);
+            using (var font = new Font("Arial", 9, FontStyle.Bold))
+            {
+                g.DrawString(tag.Contrato.ToString(), font, Brushes.Black, contratoXY);
+                g.DrawString(tag.Servico.ToString(), font, Brushes.Black, servicoXY);
+                g.DrawString(tag.Rastreamento.ToString(), font, Brushes.Black, rastreamentoXY);
+            }
 
+            using (var font = new Font("Arial", 9, FontStyle.Regular))
+            {
+                g.DrawString(tag.NF.ToString(), font, Brushes.Black, nfXY);
+                g.DrawString(tag.Pedido.ToString(), font, Brushes.Black, pedidoXY);
+                g.DrawString(tag.Peso.ToString(), font, Brushes.Black, pesoXY);
+                g.DrawString(tag.Destinatario.ToString(), font, Brushes.Black, destinatarioXY);
+
+            }
             //g.DrawString("dsadaas", new Font("Arial", 12, FontStyle.Bold), Brushes.Black, codRegistroPoint);
             var fileName = "label-" + DateTime.Now.ToString("dd-MM-yyyy-hh-mm-ss-fff") + ".jpg";
             g.Flush();
@@ -213,6 +236,24 @@ namespace Etiqueta.NET.Core
             return bmp;
         }
 
+        private static Image GerarCodBarrasCep(String cep)
+        {
+            using (var barcode = new Barcode
+            {
+                Alignment = AlignmentPositions.LEFT,
+                Width = 300,
+                RotateFlipType = RotateFlipType.RotateNoneFlipNone,
+                BackColor = Color.White,
+                ForeColor = Color.Black,
+               // LabelPosition = LabelPositions.TOPLEFT,
+               // LabelFont = new Font(FontFamily.GenericSerif, 20, FontStyle.Bold),
+                //IncludeLabel = true
+            })
+            {
+                var img = barcode.Encode(TYPE.CODE128, cep);
+                return img;
+            }
+        }
         #endregion
     }
 }
